aff1b4ab35b708874ee4659ea9bbe3116f135f76
megootang <megoo.tang@gmail.com>
2015-09-10 19:27:43 +0800

    incread filename

--- direct_write.c
+++ direct_write.c
@@ -5,18 +5,35 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
-//#define BUF_SIZE 524288
+static int usage() {
+    fprintf(stderr, "direct_write <filename> <cycle> <bufsize>\n");
+    return EXIT_FAILURE;
+}
+
 int main(int argc, char * argv[])
 {
 	int fd;
 	int ret;
 	int i =0;
-	long long cycle    = atoi(argv[1]);
-	long long BUF_SIZE  = atoi(argv[2]);
-
+	long long cycle    ;
+	long long BUF_SIZE  ;
+	char *filename ;
 	unsigned char *buf;
-	if(BUF_SIZE%512)
-		BUF_SIZE = (BUF_SIZE/512 )* 512;
+	if (argc != 4) {
+		fprintf(stderr, "Incorrect argument count\n");
+		return usage();
+	}
+
+	filename = argv[1];
+	cycle    = atoi(argv[2]);
+	BUF_SIZE = atoi(argv[3]);
+
+	if(BUF_SIZE%512) {
+		if(BUF_SIZE/512)
+			BUF_SIZE = (BUF_SIZE/512 + 1 )* 512;
+		else
+			BUF_SIZE = 512;
+	}
 	ret = posix_memalign((void **)&buf, 512, BUF_SIZE);
 	if (ret) {
 		perror("posix_memalign failed");
@@ -24,21 +41,21 @@ int main(int argc, char * argv[])
 	}
 	memset(buf, 'c', BUF_SIZE);
 
-	
-/*	fd = open("./direct_io.data", O_WRONLY | O_DIRECT | O_CREAT | O_TRUNC, 0755);*/
-	fd = open("./direct_io.data", O_WRONLY | O_DSYNC  | O_DIRECT | O_CREAT | O_LARGEFILE, 0755);
+
+//	fd = open("/mnt/shell/emulated/0/direct_io.data", O_WRONLY | O_DIRECT | O_CREAT | O_TRUNC, 0755);
+	fd = open(filename, O_WRONLY | O_DSYNC  | O_DIRECT | O_CREAT | O_LARGEFILE, 0755);
 	if (fd < 0){
 		perror("open ./direct_io.data failed");
 		exit(1);
 	}
 
 	do {
-		  ret = write(fd, buf, BUF_SIZE);
-		  if (ret < 0) {
-			  perror("write ./direct_io.data failed");
+		ret = write(fd, buf, BUF_SIZE);
+		if (ret < 0) {
+			perror("write ./direct_io.data failed");
 		}
-		  i++;
-	} while (i  < cycle);
+		i++;
+	} while (i<cycle);
         fsync(fd);
 	free(buf);
 	close(fd);
